1afcd8cf6aa86bb50d0aee40c8d79d8807b9b1c7
qiruijidian <cdqr8969@163.com>
2022-12-07 16:34:54 +0800

    调整配置参数

--- settings.py
+++ settings.py
@@ -6,17 +6,33 @@
 import platform
 
 BASE_PATH = "/home/weather"
-LOG_FILE = {"tianjin": "/home/weather/log/tianjin/logs.txt", "cona": "/home/weather/log/cona/logs.txt"}
-SIG_FILE = {"tianjin": "/home/weather/sig/tianjin.txt", "cona": "/home/weather/sig/cona.txt"}
+LOG_FILE = {
+    "tianjin": "/home/weather/log/tianjin/logs.txt",
+    "cona": "/home/weather/log/cona/logs.txt",
+    "kamba": "/home/weather/log/kamba/logs.txt"
+}
+SIG_FILE = {
+    "tianjin": "/home/weather/sig/tianjin.txt",
+    "cona": "/home/weather/sig/cona.txt",
+    "kamba": "/home/weather/sig/kamba.txt"
+}
 CONN_CONF = {"host": "121.199.48.82", "user": "root", "password": "cdqr2008", "database": "weather"}
-DATA_CHECK_PATH = {"tianjin": "/home/weather/data_check/tianjin", "cona": "/home/weather/data_check/cona"}
-TABLES = {"cona": "cona", "tianjin": "tianjin"}
+DATA_CHECK_PATH = {
+    "tianjin": "/home/weather/data_check/tianjin",
+    "cona": "/home/weather/data_check/cona",
+    "kamba": "/home/weather/data_check/kamba"
+}
+TABLES = {"cona": "cona", "tianjin": "tianjin", "kamba": "kamba"}
 
 if platform.system() == "Windows":
-    LOG_FILE = {"tianjin": "./log/tianjin/logs.txt", "cona": "./log/cona/logs.txt"}
+    LOG_FILE = {
+        "tianjin": "./log/tianjin/logs.txt",
+        "cona": "./log/cona/logs.txt",
+        "kamba": "./log/kamba/logs.txt"
+    }
     CONN_CONF = {"host": "localhost", "user": "root", "password": "cdqr2008", "database": "weather"}
-    SIG_FILE = {"tianjin": "./sig/tianjin.txt", "cona": "./sig/cona.txt"}
-    DATA_CHECK_PATH = {"tianjin": "./data_check/tianjin", "cona": "./data_check/cona"}
+    SIG_FILE = {"tianjin": "./sig/tianjin.txt", "cona": "./sig/cona.txt", "kamba": "./sig/kamba.txt"}
+    DATA_CHECK_PATH = {"tianjin": "./data_check/tianjin", "cona": "./data_check/cona", "kamba": "./data_check/kamba"}
 
 
 
